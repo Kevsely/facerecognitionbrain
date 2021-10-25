d7d84fc03a08ae4f097eb35b46dadd072cb67f87
Elegis Sossou <elegis.sossou@yahoo.com>
2021-10-25 16:17:36 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      {/* 
       <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
