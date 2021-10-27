52969edf50f4a6ac235b404c1444c28c78558d8a
Elegis Sossou <elegis.sossou@yahoo.com>
2021-10-27 16:13:17 +0100

    Adding Clarifai API

--- src/App.js
+++ src/App.js
@@ -18,16 +18,48 @@ const particlesOptions = {
   }
 }
 
+//Clarifai init
+const raw = JSON.stringify({
+  "user_app_id": {
+    "user_id": "kevsely",
+    "app_id": "f92ad747e4c44a248b810c187ba4164c"
+  },
+  "inputs": [
+    {
+      "data": {
+        "image": {
+          "url": "https://samples.clarifai.com/metro-north.jpg"
+        }
+      }
+    }
+  ]
+});
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Authorization': 'Key b2dfced0cc0a415d8c23457948834eb6'
+  },
+  body: raw
+};
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input : ""
+      input: ""
     }
   }
 
   onInputChanges = (event) => {
     console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    fetch("https://api.clarifai.com/v2/models/f76196b43bbd45c99b4f3cd8e8b40a8a/outputs", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(JSON.parse(result, null, 2).outputs[0].data))
+      .catch(error => console.log('error', error));
   }
 
   render() {
@@ -37,7 +69,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChanges={this.onInputChanges}/>
+        <ImageLinkForm
+          onInputChanges={this.onInputChanges}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/*       
       <FaceRecognition /> */}
       </div >
