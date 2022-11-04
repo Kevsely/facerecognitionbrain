8c1c146ca50b5c0ea7ce6bdd7968fe16849b3704
Kevsely <elegis.sossou@yahoo.com>
2022-11-04 13:13:33 +0100

    Updated the particles package

--- src/App.js
+++ src/App.js
@@ -7,17 +7,80 @@ import Signin from './Components/Signin/Signin';
 import Register from './Components/Register/Register';
 import Rank from './Components/Rank/Rank'
 import Particles from 'react-tsparticles';
+import { loadFull } from "tsparticles";
 import './App.css';
 
 const particlesOptions = {
-  particles: {
-    number: {
-      value: 80,
-      density: {
-        enable: true,
-        value_area: 800
+  "fullScreen": {
+      "enable": true,
+      "zIndex": 1
+  },
+  "fpsLimit": 120,
+  "particles": {
+      "number": {
+          "value": 80,
+          "density": {
+              "enable": true,
+              "value_area": 800
+          }
+      },
+      "color": {
+          "value": "#ff0000",
+          "animation": {
+              "enable": true,
+              "speed": 20,
+              "sync": true
+          }
+      },
+      "opacity": {
+          "value": 0.5
+      },
+      "size": {
+          "value": {
+              "min": 0.1,
+              "max": 3
+          }
+      },
+      "links": {
+          "enable": true,
+          "distance": 100,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+      },
+      "move": {
+          "enable": true,
+          "speed": 2,
+          "direction": "none",
+          "outModes": {
+              "default": "out"
+          }
       }
-    }
+  },
+  "interactivity": {
+      "events": {
+          "onHover": {
+              "enable": true,
+              "mode": "repulse"
+          },
+          "onClick": {
+              "enable": true,
+              "mode": "push"
+          },
+          "resize": true
+      },
+      "modes": {
+          "repulse": {
+              "distance": 200
+          },
+          "push": {
+              "quantity": 4
+          }
+      }
+  },
+  "detectRetina": true,
+  "background": {
+      "color": "transparent"
   }
 }
 
@@ -86,6 +149,15 @@ class App extends Component {
     this.state = initialState;
   }
 
+  particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
   loadUser = (data) => {
     this.setState({user: {
       id: data.id,
@@ -137,7 +209,7 @@ class App extends Component {
     const { formUrl, box, route, isSignedIn, user } = this.state;
     return (
       < div className="App" >
-        <Particles className="particle" params={particlesOptions} />
+        <Particles className="particle" init={this.particlesInit} options={particlesOptions} />
         <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
         {route === 'home' ? 
           <div>
